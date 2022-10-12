f77eb952dc78e17ec83066dcc1e3a42a52561fb6
sharmila subramaniyan <103165190+Sharmignanam@users.noreply.github.com>
2022-10-12 21:10:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// var cont = document.createElement("div");
+// cont.setAttribute("class","container");
+// var r = document.createElement("div");
+// r.setAttribute("class","row");
+// var c = document.createElement("div");
+// c.setAttribute("class","col");
+// c.innerHTML="This is col";
+// cont.append(r);
+// r.append(c);
+// //div2.append("This is col");
+// document.body.append(cont);
+// //  document.body.append(div1);
+// // document.body.append(div2);
+function foo(){
+var res=document.getElementById("fname").value;
+var res1=document.getElementById("mname").value;
+ var res2=document.getElementById("lname").value;
+ var res3=document.getElementById("email").value;
+var res4=document.getElementById("pass").value;
+ console.log(res,res1,res2,res3,res4);
+//  if(res3==="abc@guvi.in" && res4==="123"){
+//        console.log("valid user");
+//  }
+//  else{
+//          console.log("invalid user");
+//      } 
+    }
